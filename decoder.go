35ccc75bc6dbfcc7d61eb41bcc40dbc76ab5e167
Ryan Carver <ryan@ryancarver.com>
2015-03-20 19:52:08 -0700

    initial impll

---
+++ decoder.go
@@ -0,0 +1,130 @@
+package drum
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// DecodeFile decodes the drum machine file found at the provided path
+// and returns a pointer to a parsed pattern which is the entry point to the
+// rest of the data.
+func DecodeFile(path string) (*Pattern, error) {
+	p := &Pattern{}
+
+	// Open the file.
+	fi, err := os.Open(path)
+	if err != nil {
+		return p, err
+	}
+	defer fi.Close()
+
+	// Read from the file.
+	reader := bufio.NewReader(fi)
+
+	// Parse the file header.
+	header := header{}
+	trackBytes, err := decodeHeader(&header, reader)
+	if err != nil {
+		return p, err
+	}
+
+	// Set fields from the header.
+	p.Version = header.Version()
+	p.Tempo = header.Tempo
+
+	// Read the rest of the file for tracks.
+	trackReader := bufio.NewReader(io.LimitReader(reader, trackBytes))
+
+	// Parse the track data.
+	for {
+		t := Track{}
+		err := decodeTrack(&t, trackReader)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return p, err
+		}
+		p.AddTrack(&t)
+	}
+
+	return p, nil
+}
+
+// header is the low level binary header.
+type header struct {
+	Magic        [13]byte
+	FileSize     byte
+	VersionBytes [32]byte
+	Tempo        float32
+}
+
+// ValidMagic verifies if the file has the right kind of header.
+func (h header) ValidMagic() bool {
+	return string(h.Magic[0:6]) == "SPLICE"
+}
+
+// Version returns the file version string.
+func (h header) Version() string {
+	return strings.Trim(string(h.VersionBytes[:]), "\x00")
+}
+
+// decodeHeader extracts binary from reader into the header. It returns the
+// number of bytes remaining in the file for track data.
+func decodeHeader(h *header, reader io.Reader) (int64, error) {
+	// Read into the struct.
+	err := binary.Read(reader, binary.LittleEndian, h)
+	if err != nil {
+		return 0, err
+	}
+
+	// Verify the magic header.
+	if !h.ValidMagic() {
+		return 0, fmt.Errorf("Magic header is wrong, got: %s", h.Magic)
+	}
+
+	// Calculate the remaining bytes after reading everything.
+	bytes := int64(h.FileSize) - int64(len(h.VersionBytes)) - 4 /* Tempo float 32 */
+
+	return bytes, nil
+}
+
+// decodeTrack extracts binary from the reader into the Track.
+func decodeTrack(t *Track, reader io.Reader) error {
+	buf := bufio.NewReader(reader)
+
+	// Set the ID
+	err := binary.Read(buf, binary.LittleEndian, &t.ID)
+	if err != nil {
+		return err
+	}
+
+	// Get the size of the Name.
+	size, err := buf.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	// Set the Name.
+	name := make([]byte, size)
+	_, err = io.ReadFull(buf, name)
+	if err != nil {
+		return err
+	}
+	t.Name = string(name)
+
+	// Set the Steps.
+	for i := 0; i < 16; i++ {
+		b, err := buf.ReadByte()
+		if err != nil {
+			return err
+		}
+		t.Steps[i] = b == 1
+	}
+
+	return nil
+}
---
+++ drum.go
@@ -0,0 +1,55 @@
+// Package drum is supposed to implement the decoding of .splice drum machine files.
+// See golang-challenge.com/go-challenge1/ for more information
+package drum
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// Pattern is the high level representation of the
+// drum pattern contained in a .splice file.
+type Pattern struct {
+	Version string
+	Tempo   float32
+	Tracks  []*Track
+}
+
+// AddTrack gives the pattern another track to play.
+func (p *Pattern) AddTrack(t *Track) {
+	p.Tracks = append(p.Tracks, t)
+}
+
+// String outputs a human readable view of the entire pattern.
+func (p Pattern) String() string {
+	var buf bytes.Buffer
+
+	fmt.Fprintf(&buf, "Saved with HW Version: %s\n", p.Version)
+	fmt.Fprintf(&buf, "Tempo: %g\n", p.Tempo)
+
+	for _, track := range p.Tracks {
+		fmt.Fprintf(&buf, "(%d) %s\t", track.ID, track.Name)
+
+		fmt.Fprintf(&buf, "|")
+		for i, step := range track.Steps {
+			if step {
+				fmt.Fprintf(&buf, "x")
+			} else {
+				fmt.Fprintf(&buf, "-")
+			}
+			if (i+1)%4 == 0 {
+				fmt.Fprintf(&buf, "|")
+			}
+		}
+		fmt.Fprintf(&buf, "\n")
+	}
+
+	return buf.String()
+}
+
+// Track is the pattern played by a single instrument within a Pattern.
+type Track struct {
+	ID    uint32
+	Name  string
+	Steps [16]bool
+}
